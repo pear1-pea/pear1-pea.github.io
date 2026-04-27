b18e61c5906ee815c60cf5de447e807e654a2760
pear1-pea <3435565952@qq.com>
2026-04-27 18:17:19 +0800

    Initial Astro blog setup
    - Initialize Astro 5.x with blog template
    - Configure site URL for GitHub Pages
    - Add GitHub Actions workflow for auto-deployment
    - Set up Chinese blog title and description

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Pear's Blog";
+export const SITE_DESCRIPTION = 'Pear 的个人博客 - 技术、生活与思考';
